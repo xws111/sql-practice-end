04bfbe390206570d0fb523398bf2fe06f8ade537
Fancier <hf2556482879@outlook.com>
2024-06-10 18:42:01 +0800

    AdminUserController 开发-fancier

--- sql-practice-user-service/src/main/java/com/xws111/sqlpractice/user/controller/AdminUserController.java
+++ sql-practice-user-service/src/main/java/com/xws111/sqlpractice/user/controller/AdminUserController.java
@@ -1,12 +1,13 @@
 package com.xws111.sqlpractice.user.controller;  
   
-import com.fasterxml.jackson.databind.ser.Serializers;  
-import com.xws111.sqlpractice.common.BaseResponse;  
-import com.xws111.sqlpractice.common.PageRequest;  
+
+import com.xws111.sqlpractice.common.BaseResponse;
 import com.xws111.sqlpractice.common.PageResponse;  
 import com.xws111.sqlpractice.model.dto.user.*;  
-import com.xws111.sqlpractice.user.service.AdminUserService;  
-import lombok.RequiredArgsConstructor;  
+import com.xws111.sqlpractice.user.service.AdminUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,31 +20,37 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2024/6/9 15:25  
  */@RestController  
 @RequestMapping("/admin")  
-@RequiredArgsConstructor  
+@RequiredArgsConstructor
+@Api("管理端用户控制层")
 public class AdminUserController {  
     private final AdminUserService adminUserService;  
   
-    @PostMapping("/delete")  
+    @PostMapping("/delete")
+    @ApiOperation(value = "deleteUser", notes = "根据id删除用户")
     public BaseResponse deleteUser(@RequestBody UserDeleteRequest deleteRequest) {
         return adminUserService.deleteUser(deleteRequest);  
     }  
   
-    @PostMapping("/list/page")  
-    public PageResponse FuzzyPageQuery(@RequestBody UserQueryRequest userQueryRequest) {
+    @PostMapping("/list/page")
+    @ApiOperation(value = "fuzzyPageQuery",notes = "管理员分页获取用户信息")
+    public PageResponse fuzzyPageQuery(@RequestBody UserQueryRequest userQueryRequest) {
         return adminUserService.fuzzyPageQuery(userQueryRequest);  
     }  
   
-    @PostMapping("/search")  
+    @PostMapping("/search")
+    @ApiOperation(value = "searchUser",notes = "管理员模糊查询用户信息")
     public BaseResponse searchUser(@RequestBody UserSearchRequest searchRequest) {
         return adminUserService.searchUser(searchRequest);  
     }  
   
-    @PostMapping("/add")  
+    @PostMapping("/add")
+    @ApiOperation(value = "addUser", notes = "管理端添加用户")
     public BaseResponse addUser(@RequestBody UserAddRequest addRequest) {
         return adminUserService.addUser(addRequest);  
     }  
   
-    @PostMapping("/update")  
+    @PostMapping("/update")
+    @ApiOperation(value = "updateUser", notes = "管理端更新用户信息")
     public BaseResponse updateUser(@RequestBody UserUpdateRequest userUpdateRequest) {
         return adminUserService.updateUser(userUpdateRequest);  
     }  
