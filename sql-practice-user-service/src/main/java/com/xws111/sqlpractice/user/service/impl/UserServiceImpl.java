4f4592a95d020ea7c1a9535ed03f6957f2289673
xws111 <18640173215@163.com>
2024-06-11 11:58:18 +0800

    style 删除无用注释

--- sql-practice-user-service/src/main/java/com/xws111/sqlpractice/user/service/impl/UserServiceImpl.java
+++ sql-practice-user-service/src/main/java/com/xws111/sqlpractice/user/service/impl/UserServiceImpl.java
@@ -145,30 +145,8 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         if (currentUser == null || currentUser.getId() == null) {
             throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
         }
-        // 从数据库查询（追求性能的话可以注释，直接走缓存）
-//        long userId = currentUser.getId();
-//        return this.getById(userId);
         return currentUser;
     }
-
-//    /**
-//     * 是否为管理员
-//     *
-//     * @param request
-//     * @return
-//     */
-//    @Override
-//    public boolean isAdmin(HttpServletRequest request) {
-//        // 仅管理员可查询
-//        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
-//        User user = (User) userObj;
-//        return isAdmin(user);
-//    }
-
-//    @Override
-//    public boolean isAdmin(User user) {
-//        return user != null && user.getRole() == UserRoleEnum.ADMIN.getRole();
-//    }
 
     /**
      * 用户注销
