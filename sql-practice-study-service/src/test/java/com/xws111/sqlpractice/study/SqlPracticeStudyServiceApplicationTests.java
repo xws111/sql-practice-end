8eb9692cbbec9c23c8982cd29639f323f1020f9a
xws111 <18640173215@163.com>
2024-08-26 16:18:59 +0800

    feat 新增服务

---
+++ sql-practice-study-service/src/test/java/com/xws111/sqlpractice/study/SqlPracticeStudyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xws111.sqlpractice.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlPracticeStudyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
